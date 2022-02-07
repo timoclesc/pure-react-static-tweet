cdfb4b65ed387f9c59eed48500a799066a5dc432
Tim Copland <tim.copland@august.com.au>
2022-02-07 15:37:31 +1100

    Chapter 7 & 8

--- src/index.js
+++ src/index.js
@@ -1,17 +1,139 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import moment from 'moment';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Avatar, Button, Card, CardContent } from 'ui-neumorphism';
+import 'ui-neumorphism/dist/index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function Tweet({ tweet }) {
+    return (
+        <Card className="tweet--neu">
+            <CardContent>
+                <div className="flex">
+                    <AuthorAvatar hash={tweet.gravatar} />
+                    <div className="content">
+                        <Author author={tweet.author} />
+                        <Time time={tweet.timestamp} />
+                        <Message text={tweet.message} />
+                    </div>
+                </div>
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton count={tweet.retweets} />
+                    <LikeButton count={tweet.likes} />
+                    <MoreOptionsButton />
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+const testTweet = {
+    message: 'Something about cats.',
+    gravatar: 'xyz',
+    author: {
+        handle: 'catperson',
+        name: 'IAMA Cat Person'
+    },
+    likes: 5,
+    retweets: 111,
+    timestamp: new Date() - 500000
+}
+
+Tweet.propTypes = PropTypes.shape({
+    message: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.number
+}).isRequired
+
+function AuthorAvatar({ hash }) {
+    const url = `https://www.gravatar.com/avatar/${hash}`
+    return (
+        <Avatar src={url} alt="avatar" size="large" className="avatar" style={{'marginRight': '10px'}}/>
+    )
+}
+
+Avatar.propTypes = {
+    hash: PropTypes.string
+}
+function Message({ text }) {
+    return (
+        <div className="message">
+            {text}
+        </div>
+    )
+}
+
+Message.propTypes = {
+    text: PropTypes.string
+}
+
+function Author({ author }) {
+    return (
+        <span className="author">
+            <span className="name">{author.name}</span>
+            <span className="handle">@{author.handle}</span>
+        </span>
+    )
+}
+
+Author.propTypes = {
+    author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        handle: PropTypes.string.isRequired
+    }).isRequired
+}
+
+const Time = ({ time }) => {
+    const timeString = moment(time).fromNow();
+    return <span className="time">{timeString}</span>;
+}
+
+Time.propTypes = {
+    time: PropTypes.number
+}
+
+const ReplyButton = () => (
+    <Button className="reply-button">
+        <i className="fa fa-reply"></i>
+    </Button>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getRetweetCount(count) {
+    if (count > 0) {
+        return <span className="retweet-count">{count}</span>;
+    } else {
+        return null;
+    }
+}
+
+const RetweetButton = ({ count }) => (
+    <Button className="retweet-button">
+        <i className="fa fa-retweet"></i>
+        {getRetweetCount(count)}
+    </Button>
+);
+
+RetweetButton.propTypes = {
+    count: PropTypes.number
+}
+
+
+const LikeButton = ({ count }) => (
+    <Button className="like-button">
+        <i className="fa fa-heart"></i>
+        {count > 0 && <span className="like-count">{count}</span>}
+    </Button>
+);
+const MoreOptionsButton = () => (
+    <Button className="more-options-button">
+        <i className="fa fa-ellipsis-h"></i>
+    </Button>
+);
+
+LikeButton.propTypes = {
+    count: PropTypes.number
+}
+
+ReactDOM.render(<Tweet tweet={testTweet} />, document.querySelector('#root'));
